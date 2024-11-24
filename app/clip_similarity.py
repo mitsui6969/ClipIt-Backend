a23e993f7cd1e77436e2639980370151aa5eca44
naminamina <namikawa.mail.kyoto.mail@gmail.com>
2024-11-24 22:53:30 +0900

    clipのテスト

---
+++ app/clip_similarity.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import requests
+from transformers import CLIPProcessor, CLIPModel
+import torch
+import torch.nn.functional as F
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+inputs = processor(text=["a photo of a cat"], images=image, return_tensors="pt", padding=True)
+
+outputs = model(**inputs)
+image_embeds = outputs.image_embeds  
+text_embeds = outputs.text_embeds  
+
+
+cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
+similarity_percentage = cosine_similarity.item() * 100
+
+print(f"一致度 (コサイン類似度): {similarity_percentage:.2f}%")
--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from PIL import Image
+import requests
+from transformers import CLIPProcessor, CLIPModel
+import torch
+import torch.nn.functional as F
+
 
 app = FastAPI()
 
@@ -24,5 +30,21 @@ async def root():
 
 @app.post("/upload")
 async def read_user_me():
-    return {"user_id": "the current user"}
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
+
+    inputs = processor(text=["a photo of a cat"], images=image, return_tensors="pt", padding=True)
+
+    outputs = model(**inputs)
+    image_embeds = outputs.image_embeds  
+    text_embeds = outputs.text_embeds  
+
+
+    cosine_similarity = F.cosine_similarity(image_embeds, text_embeds)
+    similarity_percentage = cosine_similarity.item() * 100
+
+    print(f"一致度 : {similarity_percentage:.2f}%")
+    return {"similarity": similarity_percentage}
